a58643e045fffa2f1e850813aa0c64671eebbe02
pourzarrabi <pourzarrabi@gmail.com>
2024-08-21 01:55:55 +0330

    Structure Files and Folders

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Vazirmatn } from "next/font/google";
+import "./globals.css";
+
+const vazir = Vazirmatn({
+  subsets: ["arabic"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-vazir",
+});
+
+export const metadata: Metadata = {
+  title: "بانک یکتا",
+  description: "بانکداری حساب های بین‌الملل",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='fa' dir='rtl'>
+      <body className={`${vazir.variable}`}>{children}</body>
+    </html>
+  );
+}
